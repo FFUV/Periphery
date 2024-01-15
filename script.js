eb33de016263e53feb2814c2c766881d0ee8d0ec
Muusy <112031065+Ttrmaniac@users.noreply.github.com>
2024-01-15 11:24:24 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+function generatePi() {
+    var piValueElement = document.getElementById("pi-value");
+    piValueElement.innerHTML = "Calculating...";
+    
+    // Make a request to the server to compute pi
+    fetch("/pi")
+        .then(response => response.json())
+        .then(data => {
+            piValueElement.innerHTML = data.pi;
+        })
+        .catch(error => {
+            console.error(error);
+            piValueElement.innerHTML = "Error occurred.";
+        });
+}
